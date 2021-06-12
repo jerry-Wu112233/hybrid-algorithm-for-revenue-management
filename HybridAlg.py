62db2777572de59fa135d4eeb765b171d320ec38
Jerry Wu <yihanwu4@illinois.edu>
2021-06-11 22:22:53 -0500

    Implemented approximation methods for finding the no-purchase probability when multiple items are offered in a bundle.

--- HybridAlg.py
+++ HybridAlg.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import numpy as np
 import sympy as sp
 import random
+import math
 
 @dataclass
 class Item:
@@ -47,6 +48,16 @@ def assign_purchase_probabilities(items, lambda_const):
     return heavy_items, light_items
     
 def solve_q0_single(theta):
+    '''Computes solves the equation (4) within the paper to compute the no-purchase probability q0.
+
+    Helper method for computing the no-purchase probability.
+
+    Args:
+        theta: the quality attribute of the single item in focus.
+
+    Returns:
+        The no-purchase probability associated with S where S := {i} where i is the item in focus.     
+    '''
     q0,y = sp.symbols('q0, y')
     
     eq1 = sp.Eq(y, 1 - q0)
@@ -59,21 +70,70 @@ def solve_V(x):
     soln = sp.solve(sp.Eq(y * np.e ** (y / (1 - y)), x), y)
     return soln[0][0].evalf()
 
-def solve_bundle_probabilities(light_items):
+def bisection(f,a,b,N, light_items):
+    '''Approximate solution of f(x)=0 on interval [a,b] by bisection method.
+    Found from https://secure.math.ubc.ca/~pwalls/math-python/roots-optimization/bisection/
+    Parameters
+    ----------
+    f : function
+        The function for which we are trying to approximate a solution f(x)=0.
+    a,b : numbers
+        The interval in which to search for a solution. The function returns
+        None if f(a)*f(b) >= 0 since a solution is not guaranteed.
+    N : (positive) integer
+        The number of iterations to implement.
 
-    pass
+    Returns
+    -------
+    x_N : number
+        The midpoint of the Nth interval computed by the bisection method. The
+        initial interval [a_0,b_0] is given by [a,b]. If f(m_n) == 0 for some
+        midpoint m_n = (a_n + b_n)/2, then the function returns this solution.
+        If all signs of values f(a_n), f(b_n) and f(m_n) are the same at any
+        iteration, the bisection method fails and return None.
+    '''
+  
+    a_n = a
+    b_n = b
+    for n in range(1,N+1):
+        m_n = (a_n + b_n)/2
+        f_m_n = f(m_n,light_items)
+        if f(a_n, light_items) * f_m_n < 0:
+            a_n = a_n
+            b_n = m_n
+        elif f(b_n, light_items) * f_m_n < 0:
+            a_n = m_n
+            b_n = b_n
+        elif f_m_n == 0:
+            return m_n
+        else:
+            return None
+
+    return (a_n + b_n) / 2
+
+def f(q0, light_items):
+    function_value = 0
+    for item in light_items:
+        function_value += solve_V(q0 * np.e ** (item.quality - 1))
+
+    return function_value - 1 + q0
+
+def update_bundle_probabilities(light_items):
+    q0_approx = bisection(f, 0, 1, 10000, light_items)
+    for item in light_items:
+        item.purchase_probability = solve_V(q0_approx * np.e ** (item.quality - 1))
+    return q0_approx
 
 def simulate(heavy_items, light_items, m_const):
     sold_items = []
     for _ in m_const:
-        if heavy_items:
+        if heavy_items:                                                             # Phase 1 of the algorithm: offering heavy items one by one
             if random.uniform(0, 1) <= heavy_items[0].purchase_probability:
                 sold_items.append(heavy_items.pop())
-            else:
-                continue    
-        else:
+              
+        else:                                                                       # Phase 2 of the algorithm: offering light items altogether
             if light_items:
-                continue
-                pass
-            
-        
+                q0_approx = update_bundle_probabilities(light_items)
+                probs = (o.purchase_probability for o in light_items)
+                probs.append(q0_approx)
+                item_bought = np.random.choice(light_items, probs)
